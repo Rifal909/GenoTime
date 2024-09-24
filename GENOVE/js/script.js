ffa86fff49e96f30407a4f478a89a652bd2d8811
Rifal909 <thrflthr0113@gmail.com>
2024-09-24 14:29:02 +0700

    Add files via upload

---
+++ GENOVE/js/script.js
@@ -0,0 +1,19 @@
+// toggle
+
+const navbarNav = document.querySelector
+('.navbar-nav');
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+// klik diluar sidebar
+
+const hamburger = document.querySelector
+('#hamburger-menu');
+
+document.addEventListener('click', function(e){
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
+        navbarNav.classList.remove('active');
+    }
+
+})
